f917e6f6c53c91cf9b4019d16ecb72f7ce04c518
fdjunaidi <fdjunaidi@berkeley.edu>
2013-02-19 11:16:07 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CS169WarmupFinal::Application.config.secret_token = '11f991dd924a851b046877c15c473022dca9c590ed75e817f294963d962d083d60cd34737e341f49bce63440a59e6ac95b263afca6eaa6c1daffb29a39793d8a'
